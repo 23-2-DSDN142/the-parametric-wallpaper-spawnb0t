0d9551c5ee4e30a0091cce8b9555963db3533738
spawnb0t <139717697+spawnb0t@users.noreply.github.com>
2023-07-24 10:27:31 +1200

    Added random drawing

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,10 +1,14 @@
 //your parameter variables go here!
-let rect_width  = 20;
-let rect_height = 20;
+let headX = 75;
+let headY = 90;
+
+  let eyeY = headY - 10;
+  let earY = headY - 40;
+  let eyeSize = 10;
 
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
@@ -18,6 +22,24 @@ function wallpaper_background() {
   background(240, 255, 240); //light honeydew green colour
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  rect(40 ,40, rect_width, rect_height);
+function my_symbol() { // do not rename this function. Draw below:
+  
+  fill(255);
+  stroke(71, 102, 155); // blue colour
+
+
+  ellipse(headX - 15, earY, 20, 70); // left ear
+  ellipse(headX + 15, earY, 20, 70);
+
+  ellipse(headX, headY, 70, 70); //head
+
+  fill(71, 102, 155) // blue colour
+
+  ellipse(headX - 15, eyeY, eyeSize, eyeSize) //left eye // 60 before
+
+  ellipse(headX + 15, eyeY, eyeSize, eyeSize) // right eye
+
+  line(headX - 5, headY + 10, headX + 5, headY + 5);
+  line(headX + 5, headY + 10, headX - 5, headY + 5);
 }
+
