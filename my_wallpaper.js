570a70f579d2191c963a23f036f199e421599c6f
spawnb0t <139717697+spawnb0t@users.noreply.github.com>
2023-07-27 10:49:09 +1200

    Adding more parameters

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,11 +1,4 @@
 //your parameter variables go here!
-let headX = 75;
-let headY = 90;
-
-  let eyeY = headY - 10;
-  let earY = headY - 40;
-  let eyeSize = 10;
-
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GRID_WALLPAPER);
@@ -19,75 +12,114 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240); //light honeydew green colour
+  background('#c9dfc8'); //pastelGreen #c9dfc8 //lightOrange #ffeeda
 }
 
 function my_symbol() { // do not rename this function. Draw below:
   
-  let black = (0)
-    let darkFur = (50)
-    let noseHoles = (40)
-    let white = (250)
+    let black = (0);
+    let mainFur = ('#a9b6bb'); // 50 dark grey //'#b97e43' tan
+    let noseHoles = (40);
+    let white = (250);
   
-    let headTop = (70);
-    let headBottom = (160)
+    let headTop = (40);
+    let headL = (80);
+      let headR = (headL+40);
+    let snoutBottom = (150);
 
-    let earStart = (75)
-    let earTop = (60)
-    let earDroop = (80)
+    let earTop = (headTop-8);
+      let earTriangleDroop = (headTop+25); //+25
+    let earLstart = (72);
+    let earRstart = (127);
 
-    let eyeL = (85)
-    let eyeR = (115)
-    let eyeHeight = (90)
+    let eyeL = (85);
+    let eyeR = (115);
+      let eyeHeight = (headTop+25); //+25
+    let eyeDiameter = (23); 
+    let pupilDiameter = (4); //4
   
     
-    // snout
-    fill(darkFur);
+    // head & snout
+    fill(mainFur);
+    noStroke();
     beginShape(); 
-    vertex(80, headTop);
-    vertex(120, headTop);
-    vertex(105, headBottom);
-    vertex(95, headBottom);
-    vertex(80, headTop);
+    vertex(headL, headTop);
+    vertex(headR, headTop);
+    vertex(headR-15, snoutBottom);
+    vertex(headL+15, snoutBottom);
+    vertex(headL, headTop);
     endShape(CLOSE);
 
     // nose
     fill(black);
-    ellipse(100, headBottom, 10, 8); 
+    ellipse(100, snoutBottom, 10, 8); 
     fill(noseHoles);
-    ellipse(98, headBottom, 3, 2); 
-    ellipse(102, headBottom, 3, 2); 
+    ellipse(98, snoutBottom+1, 3, 2); 
+    ellipse(102, snoutBottom+1, 3, 2); 
 
 
     // earLeft
-    fill(darkFur);
+    fill(mainFur);
+    noStroke();
     beginShape(); 
-    vertex(earStart, headTop);
-    vertex(earStart+10, headTop);
-    vertex(earStart+7, earTop);
-    vertex(earStart-3, earTop);
-    vertex(earStart-10, earDroop);
-    vertex(earStart-5, earDroop)
+    // L parallelogram
+    vertex(earLstart, earTop);
+    vertex(earLstart+13, earTop);
+    vertex(earLstart+20, headTop);
+    vertex(earLstart-2, headTop);
+    endShape(CLOSE);
+    // L triangle ear droop
+    beginShape(); 
+    vertex(earLstart, earTop);
+    vertex(earLstart+11, headTop)
+    vertex(earLstart-6, earTriangleDroop)
+    vertex(earLstart-9, earTriangleDroop-6)
+    endShape(CLOSE);
+    // L ear hole
+    fill(black);
+    beginShape();
+    vertex(earLstart+3, earTop+7);
+    vertex(earLstart+1, earTop+13);
+    vertex(earLstart+10, earTop+7);
     endShape(CLOSE);
 
     // earRight
-    fill(darkFur);
+    fill(mainFur);
     beginShape(); 
-    vertex(earStart+50, headTop);
-    vertex(earStart+50-10, headTop);
-    vertex(earStart+50-7, earTop);
-    vertex(earStart+50+3, earTop);
-    vertex(earStart+50+10, earDroop);
-    vertex(earStart+50+5, earDroop)
+    // R parallelogram
+    vertex(earRstart, earTop);
+    vertex(earRstart-13, earTop);
+    vertex(earRstart-16, headTop);
+    vertex(earRstart+2, headTop);
+    endShape(CLOSE);
+    // R triangle ear droop
+    beginShape(); 
+    vertex(earRstart, earTop);
+    vertex(earRstart-11, headTop)
+    vertex(earRstart+6, earTriangleDroop)
+    vertex(earRstart+9, earTriangleDroop-4)
+    endShape(CLOSE);
+    // R ear hole
+    fill(black);
+    beginShape();
+    vertex(earRstart-3, earTop+7);
+    vertex(earRstart-1, earTop+13);
+    vertex(earRstart-10, earTop+7);
     endShape(CLOSE);
 
-    //eyes
+    // eyes
     fill(white);
-    ellipse(eyeL, eyeHeight, 20, 20);
-    ellipse(eyeR, eyeHeight, 20, 20);
+    stroke(black);
+    ellipse(eyeL, eyeHeight, eyeDiameter, eyeDiameter);
+    ellipse(eyeR, eyeHeight, eyeDiameter, eyeDiameter);
     fill(black);
-    ellipse(eyeL+3, eyeHeight, 3, 3);
-    ellipse(eyeR-3, eyeHeight, 3, 3);
+    ellipse(eyeL+3, eyeHeight, pupilDiameter, pupilDiameter);
+    ellipse(eyeR-3, eyeHeight, pupilDiameter, pupilDiameter);
     
 }
+
+// fill
+// stroke (colour)
+// strokeWeight
+// strokeJoin (ROUND)
 
