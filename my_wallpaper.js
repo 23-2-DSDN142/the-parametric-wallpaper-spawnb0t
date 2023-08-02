59643b4114330c1c6d363a0deb889d3e4eabf27d
spawnb0t <139717697+spawnb0t@users.noreply.github.com>
2023-08-03 10:04:56 +1200

    Background If statements

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,37 +1,41 @@
 //your parameter variables go here!
 
+
+//colour variable "NAME" is changed to (r, g, b) at line X
+
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(GRID_WALLPAPER); // DEVELOP_GLYPH
   pWallpaper.resolution(FIT_TO_SCREEN);
   pWallpaper.show_guide(true); //set this to false when you're ready to print
 
   //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  = 50;
+  pWallpaper.grid_settings.row_offset  = 0;
 }
 
 function wallpaper_background() {
-  background('#c9dff8'); //pastelGreen #c9dfc8 //lightOrange #ffeeda
+  background('#fef7e2'); //pastelGreen #c9dfc8 //lightOrange #ffeeda
 }
 
 
 
 function my_symbol() { // do not rename this function. Draw below:
   
-      
+  angleMode(DEGREES);
   
     let black = (0);
     let mainFur = ('#b97e43'); // 50 dark grey //'#b97e43' tan // #a9b6bb light grey
     let noseHoles = (40);
     let white = (250);
-    let pink = ('#FDA2C6');
+    let tonguePink = ('#d37074');
     let eyeColour = ('#67443b'); // brown eyes
+    let turquoise = ('#4aa38f');
 
     let headTop = (40);
     let headL = (80);
       let headR = (headL+40);
-    let snoutBottom = (150);
+    let snoutBottom = (140);
 
     let earTop = (headTop-8);
       let earTriangleDroop = (headTop+25); //+25
@@ -47,14 +51,115 @@ function my_symbol() { // do not rename this function. Draw below:
 
     let midX = 100
 
+    let colourPallete = "retro";
+    let shadeBackground;
+    let shadeOne;
+    let shadeTwo;
+    let shadeThree;
+    let shadeFour;
 
-    // decorations
-    arc(0, 0, 50, 50, 0, HALF_PI);
+    if(colourPallete == "pink") {
+      
+      shadeBackground = color(0)
+      shadeOne = color(221, 110, 203);
+      shadeTwo = color(207, 135, 196);
+      shadeThree = color(240, 187, 232);
+      shadeFour = color(249, 171, 237);
+    }
+
+    else if(colourPallete == "green") {
+      shadeBackground = color(0)
+      shadeOne = color(125, 185, 132);
+      shadeTwo = color(153, 234, 161);
+      shadeThree = color(45, 115, 52);
+      shadeFour = color(187, 243, 193);
+    }
+
+    else if(colourPallete == "retro") {
+      shadeBackground = color(0)
+      shadeOne = color('#264653');
+      shadeTwo = color('#F4A261');
+      shadeThree = color('#E76F51');
+      shadeFour = color('#E9C46A');
+    }
+
+    let blobs = (1); // choose blobs 1, 2 or 3
+    // blobs 1 = touching edges - attempting continuity 
+    // blobs 2 = no touch edges
+    // blobs 3 = pixels
+
+
+    if(blobs == 1) {
+      fill(shadeOne);
+      ellipse(20, 20, 30, 30);
+      // fill(shadeOne);
+      // noStroke();
+      // beginShape();
+      // curveVertex(30, 30);
+      // curveVertex(30, 30);
+      // curveVertex(50, 50);
+      // curveVertex(60, 30);
+      // curveVertex(30, 30);
+      // curveVertex(30, 30);
+      // endShape();
+
+    }
+
+    else if(blobs == 2) {
+      fill(shadeOne);
+      ellipse(40, 40, 30, 30);
+
+    }
+
+    else {
+      fill(shadeOne);
+      ellipse(60, 60, 30, 30);
+
+    }
+
+    
+
+    //blob sketches
+    // fill(turquoise);
+    // stroke(turquoise);
+    // beginShape();
+    // curveVertex(0, 30);
+    // curveVertex(0, 30);
+    // curveVertex(30, 35);
+    // curveVertex(30, 0);
+    // curveVertex(30, 0);
+    // endShape();
+
+    // beginShape(); 
+    // vertex(0, 0);
+    // vertex(30, 0)
+    // vertex(0, 30)
+    // endShape(CLOSE);
+
+    // arc(0, 0, 50, 50, 0, 90);
+
+    // stroke(black);
+    // strokeWeight(5);
+    // vertex(10, 10);
+    // vertex(15, 20);
+
+    // stroke(black);
+    // beginShape();
+    // curveVertex(10, 10);
+    // curveVertex(10, 10);
+    // curveVertex(5, 15);
+    // // curveVertex(20, 20);
+    // // curveVertex(15, 15);
+    // // curveVertex(15, 15);
+    // endShape();
+
+    
 
 
     // tongue - mouth
+    strokeWeight(1);
     noStroke();
-    fill(pink);
+    fill(tonguePink);
     beginShape();
     curveVertex(midX, snoutBottom-20);
     curveVertex(midX, snoutBottom-20);
@@ -85,44 +190,44 @@ function my_symbol() { // do not rename this function. Draw below:
     endShape(CLOSE);
 
 
-    // // face markings
-    // // bottom R snout
-    // noStroke();
-    // fill(white);
-    // beginShape();
-    // curveVertex(midX-20, 120);
-    // curveVertex(midX-20, 120);
-    // curveVertex(108, 125);
-    // curveVertex(107, 140);
-    // curveVertex(105, 150);
-    // curveVertex(110, 150);
-    // curveVertex(110, 150);
-    // endShape();
-    // // // bottom L snout
-    // noStroke();
-    // fill(white);
-    // beginShape();
-    // curveVertex(100, 130);
-    // curveVertex(100, 130);
-    // curveVertex(93, 135);
-    // curveVertex(93, 140);
-    // curveVertex(94, 148)
-    // curveVertex(100, 150);
-    // curveVertex(100, 150);
-    // endShape();
-    // // long middle snout stroke
-    // noStroke();
-    // fill(white);
-    // beginShape();
-    // curveVertex(100, 41);
-    // curveVertex(100, 41);
-    // curveVertex(101, 40);
-    // curveVertex(103, 40);
-    // curveVertex(104, 48);
-    // curveVertex(104, 131);
-    // curveVertex(100, 131);
-    // curveVertex(100, 131);
-    // endShape();
+    // // // face markings
+    // // // bottom R snout
+    // // noStroke();
+    // // fill(white);
+    // // beginShape();
+    // // curveVertex(midX-20, 120);
+    // // curveVertex(midX-20, 120);
+    // // curveVertex(108, 125);
+    // // curveVertex(107, 140);
+    // // curveVertex(105, 150);
+    // // curveVertex(110, 150);
+    // // curveVertex(110, 150);
+    // // endShape();
+    // // // // bottom L snout
+    // // noStroke();
+    // // fill(white);
+    // // beginShape();
+    // // curveVertex(100, 130);
+    // // curveVertex(100, 130);
+    // // curveVertex(93, 135);
+    // // curveVertex(93, 140);
+    // // curveVertex(94, 148)
+    // // curveVertex(100, 150);
+    // // curveVertex(100, 150);
+    // // endShape();
+    // // // long middle snout stroke
+    // // noStroke();
+    // // fill(white);
+    // // beginShape();
+    // // curveVertex(100, 41);
+    // // curveVertex(100, 41);
+    // // curveVertex(101, 40);
+    // // curveVertex(103, 40);
+    // // curveVertex(104, 48);
+    // // curveVertex(104, 131);
+    // // curveVertex(100, 131);
+    // // curveVertex(100, 131);
+    // // endShape();
 
 
 
